26f0a92f02f5cd943ca728fd6adb078ce6902b57
Masked Avus <maskedavus@gmail.com>
2025-11-13 19:21:36 -0600

    Remove commented-out functions in symbols/types.ts

--- src/symbols/types.ts
+++ src/symbols/types.ts
@@ -1,19 +1,6 @@
 export type Utf16Code = number;
 export type Utf16Symbol = string;
 
-/*
-export function toHexNumber(value: string) {
-    const hexValue = Buffer.from(value).toString("hex");
-    return Number(hexValue);
-}
-*/
-
-/*
-export function areEqual(left: Utf16Code, right: Utf16Code) {
-    return left === right;
-}
-*/
-
 export const BLANK_SYMBOL = " ";
 
 export function isValidLength(symbol: Utf16Symbol): boolean {
